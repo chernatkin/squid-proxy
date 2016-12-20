c39c5d13aa199d0b4713b1a132da25134815c373
Sergey Chernatkin <tchers@rambler.ru>
2016-12-21 00:40:29 +0300

    small fixes

--- init_proxy.sh
+++ init_proxy.sh
@@ -6,32 +6,26 @@ passwd squid
 
 su - squid
 
-sudo add-apt-repository "deb-src http://archive.ubuntu.com/ubuntu xenial main restricted universe"
-sudo add-apt-repository "deb-src http://archive.ubuntu.com/ubuntu xenial-updates main restricted universe"
-sudo add-apt-repository "deb-src http://security.ubuntu.com/ubuntu xenial-security main restricted universe multiverse"
-sudo add-apt-repository "deb-src http://archive.canonical.com/ubuntu xenial partner"
-
-
 sudo apt-get update
 
 sudo apt-get build-dep squid3
 sudo apt-get build-dep openssl
 sudo apt-get install libssl-dev
 sudo apt-get install openssl
-sudo apt-get install squid
+sudo apt-get install apache2-utils
 
 #bug in squid requires such workaround
 sudo chmod 777 /tmp
+sudo mkdir /var/logs
 sudo chmod 777 /var/logs
 
 wget http://www.squid-cache.org/Versions/v3/3.5/squid-3.5.21.tar.gz
 tar xzf squid-3.5.21.tar.gz
+cd squid-3.5.21
 ./configure --prefix=/usr --includedir=${prefix}/include --enable-ssl --mandir=${prefix}/share/man --infodir=${prefix}/share/info --sysconfdir=/etc --localstatedir=/var --libexecdir=${prefix}/lib/squid3 --disable-maintainer-mode --disable-dependency-tracking --srcdir=. --datadir=/usr/share/squid3 --sysconfdir=/etc/squid3 --mandir=/usr/share/man --enable-inline --enable-async-io=8 --enable-storeio=ufs,aufs,diskd --enable-removal-policies=lru,heap --enable-delay-pools --enable-cache-digests --enable-underscores --enable-icap-client --enable-follow-x-forwarded-for --enable-basic-auth-helpers=MSNT,NCSA,SASL,SMB,YP,getpwnam,multi-domain-NTLM --enable-ntlm-auth-helpers=SMB --enable-digest-auth-helpers=password --with-filedescriptors=65536 --with-default-user=proxy --enable-epoll --enable-linux-netfilter -with-openssl=/usr/include/openssl/
 make
-make install
+sudo make install
 
-
-#openssl req -new -newkey rsa:2048 -sha256 -days 365 -nodes -x509 -extensions v3_ca -keyout proxyKey.pem -out proxyCert.pem
 
 #gen CA, rootCA.pem should be installed in browser
 openssl genrsa -out rootCA.key 2048
@@ -65,4 +59,8 @@ sudo iptables -t filter -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
 sudo iptables -t filter -P INPUT DROP
 
 sudo iptables -t filter -P FORWARD DROP
+
+sudo iptables-save > /etc/iptables.rules
+sudo cp iptablesload /etc/network/if-pre-up.d/
+sudo chmod +x /etc/network/if-pre-up.d/iptablesload
 
